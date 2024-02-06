99ef0dddb3d5fff6db96ef4439b61e80c7759ade
darideveloper <darideveloper@gmail.com>
2024-02-06 12:23:35 -0600

    feat (socials_chatbots/models/TelegramToken): update webwook when save token

--- socials_chatbots/models.py
+++ socials_chatbots/models.py
@@ -1,5 +1,11 @@
+import os
+from dotenv import load_dotenv
 from django.db import models
 from assistent_chatgpt.models import Business
+import requests
+
+load_dotenv()
+HOST = os.getenv("HOST")
 
 
 class TelegramToken(models.Model):
@@ -8,3 +14,17 @@ class TelegramToken(models.Model):
 
     def __str__(self):
         return self.token
+    
+    def save(self, *args, **kwargs):
+        
+        # Generate webhook
+        dynamic_webhook = f"{HOST}/webhook/telegram/{self.business.name}"
+        url = f"https://api.telegram.org/bot{self.token}" \
+            f"/setWebhook?url={dynamic_webhook}"
+        
+        # Send request to save webhook
+        res = requests.get(url)
+        if res.status_code != 200:
+            raise Exception("Error setting webhook")
+            
+        super(TelegramToken, self).save(*args, **kwargs)
